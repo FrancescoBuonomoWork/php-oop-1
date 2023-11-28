d499ad23566ec3628b7ef7ad50c30723b5f4aa52
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-28 15:13:38 +0100

    add try foreach loops

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ class Production {
         if(is_numeric($rating)&& $rating >= 0 ){
             $this->rating = intval($rating);
         } else {
-            $this->rating = 'voto non inserito';
+            $this->rating = 'voto non inserito o non valido';
         }
     }
     function __construct($title,$language,$rating)
@@ -27,3 +27,49 @@ $ritornoAlCrimine = new Production('Ritorno Al Crimine','it',8);
 // $ritornoAlCrimine->language = 'it';
 // $ritornoAlCrimine->rating = 8;
 var_dump($ritornoAlCrimine);
+
+$ilSignoreDegliAnelli = new Production('Il Signore Degli Anelli','en',9);
+var_dump($ilSignoreDegliAnelli);
+
+$ilCieloSopraBerlino = new Production('Il Cielo Sopra Berlino','de',7);
+var_dump($ilSignoreDegliAnelli);
+
+$movies=[
+    $ritornoAlCrimine,
+    $ilSignoreDegliAnelli,
+    $ilCieloSopraBerlino
+];
+var_dump($movies);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <?php
+                            foreach($movies as $index => $movie ){
+                                ?>
+                                <ul class="card">
+                                
+                                <li><?php echo $movie[$title] ?></li>
+                                  
+                                </ul>
+                                <?php
+                            }
+                            ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
